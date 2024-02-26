d843a82082aad33a4da93b1997fb2fec1381b2f5
Evans <manotievance@gmail.com>
2024-02-26 12:45:49 +0300

    created login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import StudentDashboard from './pages/StudentDashboard'
+import AdminDashboard from './pages/AdminDashboard'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div>
-    </div>
-  );
+    <Routes>
+        <Route path='/' element={<StudentDashboard />}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/admin' element={<AdminDashboard />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
