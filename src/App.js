6dfaffb9ae418104631fd7f554f8ada31687ab60
Your Name <kkamadi567@gmail.com>
2024-02-27 11:00:32 +0300

    students page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
 import StudentDashboard from './pages/StudentDashboard';
+import StudentPage from './pages/StudentPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/student" element={<StudentPage />} />
       </Routes>
     </Router>
   );
